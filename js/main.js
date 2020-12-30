2f4f6ea2d9c278bfeb03d0b9f42654e1f3449900
jeffberlin <jeff@jbrally.com>
2020-12-30 14:24:39 -0500

    Building AngularJS array for product cards

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var app = angular.module('fullApp', []);
+app.controller('appCtrl', function($scope, $document) {
+  $scope.productCardInfo = [
+    {
+      title: 'Product Title',
+      price: '$9.95',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: './images/image-placeholder-350x350.png',
+      altText: 'Product Image'
+    },
+    {
+      title: 'Product Title',
+      price: '$9.95',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: './images/image-placeholder-350x350.png',
+      altText: 'Product Image'
+    },
+    {
+      title: 'Product Title',
+      price: '$9.95',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: './images/image-placeholder-350x350.png',
+      altText: 'Product Image'
+    },
+    {
+      title: 'Product Title',
+      price: '$9.95',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: './images/image-placeholder-350x350.png',
+      altText: 'Product Image'
+    },
+    {
+      title: 'Product Title',
+      price: '$9.95',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: './images/image-placeholder-350x350.png',
+      altText: 'Product Image'
+    },
+    {
+      title: 'Product Title',
+      price: '$9.95',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: './images/image-placeholder-350x350.png',
+      altText: 'Product Image'
+    },
+  ]
+})
