84e86fa700837ed372602765d51909be5889f193
jeffberlin <jeff@jbrally.com>
2020-12-30 15:11:58 -0500

    Adding carousel to cards

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var app = angular.module('fullApp', []);
 app.controller('appCtrl', function($scope, $document) {
   $scope.productCardInfo = [
     {
-      title: 'Product Title',
-      price: '$9.95',
+      title: 'Jura Capresso Nespresso Service Tool Bit for Magnetic Handle',
+      price: '$10.95',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/image-placeholder-350x350.png',
+      image: './images/service-tool-bit.jpg',
       altText: 'Product Image'
     },
     {
