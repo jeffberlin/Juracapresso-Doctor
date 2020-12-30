c1abc01e5e40c233bd27db2dfd06200c202b3359
jeffberlin <jeff@jbrally.com>
2020-12-30 16:15:55 -0500

    Building carousel. Controls bug

--- js/main.js
+++ js/main.js
@@ -6,14 +6,16 @@ app.controller('appCtrl', function($scope, $document) {
       price: '$10.95',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
       image: './images/service-tool-bit.jpg',
-      altText: 'Product Image'
+      imageTwo: './images/service-tool-bit-2.jpg',
+      altText: 'Jura Capresso Nespresso Service Tool Bit for Magnetic Handle'
     },
     {
-      title: 'Product Title',
-      price: '$9.95',
+      title: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits',
+      price: '$24.95',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/image-placeholder-350x350.png',
-      altText: 'Product Image'
+      image: './images/shop-tool-kit-oval-head.jpg',
+      imageTwo: './images/shop-tool-kit-oval-head-2.jpg',
+      altText: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits'
     },
     {
       title: 'Product Title',
