f35bb56e768c5c4246bc904a871dd45fdfb99ee6
jeffberlin <jeff@jbrally.com>
2020-12-31 12:17:36 -0500

    Trying to build carousel image array

--- js/main.js
+++ js/main.js
@@ -5,45 +5,17 @@ app.controller('appCtrl', function($scope, $document) {
       title: 'Jura Capresso Nespresso Service Tool Bit for Magnetic Handle',
       price: '$10.95',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/service-tool-bit.jpg',
-      imageTwo: './images/service-tool-bit-2.jpg',
+      images: ['./images/service-tool-bit.jpg', './images/service-tool-bit-2.jpg'],
+      // imageTwo: './images/service-tool-bit-2.jpg',
       altText: 'Jura Capresso Nespresso Service Tool Bit for Magnetic Handle'
     },
     {
       title: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits',
       price: '$24.95',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/shop-tool-kit-oval-head.jpg',
-      imageTwo: './images/shop-tool-kit-oval-head-2.jpg',
+      images: ['./images/shop-tool-kit-oval-head.jpg', './images/shop-tool-kit-oval-head-2.jpg'],
+      // imageTwo: './images/shop-tool-kit-oval-head-2.jpg',
       altText: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits'
-    },
-    {
-      title: 'Product Title',
-      price: '$9.95',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/image-placeholder-350x350.png',
-      altText: 'Product Image'
-    },
-    {
-      title: 'Product Title',
-      price: '$9.95',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/image-placeholder-350x350.png',
-      altText: 'Product Image'
-    },
-    {
-      title: 'Product Title',
-      price: '$9.95',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/image-placeholder-350x350.png',
-      altText: 'Product Image'
-    },
-    {
-      title: 'Product Title',
-      price: '$9.95',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: './images/image-placeholder-350x350.png',
-      altText: 'Product Image'
     },
   ]
 })
