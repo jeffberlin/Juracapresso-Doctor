7903eb9415ecba0863993b75a6090a8d527d8f1a
Jeff Berlin <jeff@jbrally.com>
2021-01-21 14:53:05 -0500

    Adding product page

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/service-tool-bit.jpg','./images/service-tool-bit-2.jpg'],
       altText: 'Jura Capresso Nespresso Service Tool Bit for Magnetic Handle',
       productid: '39902',
-      modalName: 'serviceToolBit',
-      modalTitle: 'Jura Capresso Nespresso Service Tool Bit for Magnetic Handle',
     },
     {
       title: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits',
@@ -21,8 +19,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/shop-tool-kit-oval-head.jpg', './images/shop-tool-kit-oval-head-2.jpg', './images/shop-tool-kit-oval-head-3.jpg', './images/shop-tool-kit-oval-head-4.jpg',],
       altText: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits',
       productid: '39912',
-      modalName: 'shopToolKit',
-      modalTitle: 'Jura Nespresso Professional Shop Tool Kit - Oval head w/ WIHA Handle & Torx bits',
     },
     {
       title: 'Jura Capresso Service Repair Tool Key - Open Security Oval Head Screws',
@@ -32,8 +28,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/service-repair-key.jpg', './images/service-repair-key-2.jpg', './images/service-repair-key-3.jpg', './images/service-repair-key-4.jpg', './images/service-repair-key-5.jpg'],
       altText: 'Jura Capresso Service Repair Tool Key - Open Security Oval Head Screws',
       productid: '39901',
-      modalName: '',
-      modalTitle: '',
     },
     {
       title: 'Jura Capresso OEM Brew Group O-ring set w/Oval Head Service Key Tool',
@@ -43,8 +37,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/o-ring-set-key-tool.jpg', './images/o-ring-set-key-tool-2.jpg'],
       altText: 'Jura Capresso OEM Brew Group O-ring set w/Oval Head Service Key Tool',
       productid: '39910',
-      modalName: '',
-      modalTitle: '',
     },
     {
       title: 'Jura Capresso OEM Brew Group O-ring gasket seal set (no service tool)',
@@ -54,8 +46,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/o-ring-set.jpg', './images/o-ring-set-2.jpg'],
       altText: 'Jura Capresso OEM Brew Group O-ring gasket seal set (no service tool)',
       productid: '39909',
-      modalName: '',
-      modalTitle: '',
     },
     {
       title: 'Duplex Decora insert for Neutrik "D" Series XLR Cannon or SpeakOn Paintable',
@@ -65,8 +55,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/duplex-decora-insert.jpg', './images/duplex-decora-insert-2.jpg'],
       altText: 'Duplex Decora insert for Neutrik "D" Series XLR Cannon or SpeakOn Paintable',
       productid: '',
-      modalName: '',
-      modalTitle: '',
     },
     {
       title: 'Espresso machine brew group O-ring seal grease DOW CORNING MOLYKOTE 111',
@@ -76,8 +64,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/dow-corning-grease.jpg', './images/dow-corning-grease-2.jpg'],
       altText: 'Espresso machine brew group O-ring seal grease DOW CORNING MOLYKOTE 111',
       productid: '39904',
-      modalName: '',
-      modalTitle: '',
     },
     {
       title: 'Jura Capresso OEM Cherry D45X Micro Switch (for Grounds/Tray Error)',
@@ -87,8 +73,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/cherry-micro-switch.jpg', './images/cherry-micro-switch-2.jpg'],
       altText: 'Jura Capresso OEM Cherry D45X Micro Switch. Fix EMPTY GROUNDS/TRAY MISSING error',
       productid: '39906',
-      modalName: '',
-      modalTitle: '',
     },
     {
       title: 'Decora insert for Neutrik "D" Series XLR (Cannon) or SpeakOn Paintable',
@@ -98,8 +82,6 @@ app.controller('appCtrl', function($scope, $document) {
       images: ['./images/duplex-decora-insert-single.jpg', './images/duplex-decora-insert-single-2.jpg'],
       altText: 'Decora insert for Neutrik "D" Series XLR (Cannon) or SpeakOn Paintable',
       productid: '',
-      modalName: '',
-      modalTitle: '',
     }
   ];
 
